a38811f3468781e84a4fb4e5be61859a3a08a122
Kohpai <whcwhc78@gmail.com>
2015-03-14 04:00:09 +0700

    change project

---
+++ index.php
@@ -0,0 +1,286 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;" />
+		<meta name="language" content="th" />
+		<title>CE SMART CAMP 8</title>
+		<link rel="stylesheet" href="style.css">
+		<script type="text/javascript" src="scripts/jquery.min.js"></script>
+		<script type="text/javascript" src="scripts/skrollr.min.js"></script>
+		<script type="text/javascript">
+			$(document).ready(function(){
+				var s = skrollr.init();
+			})
+		</script>
+	</head>
+	<body >
+		<div class="nv_group">
+			<a href="">
+				<svg x="0px" y="0px" width="170px" height="26px" viewBox="0 0 170 26">
+					<polygon fill="#000000" fill="none" points="0,0 170,0 160,26 0,26">
+					</polygon>
+					<text x="155" y="22" fill="#FFFFFF" text-anchor="end">CE SMART CAMP 8</text>
+				</svg>
+			</a>
+	<!-- 		<a href="">
+				<svg x="0px" y="0px" width="170px" height="26px" viewBox="0 0 170 26">
+					<polygon fill="#000000" fill="none" points="0,0 170,0 160,26 0,26">
+					</polygon>
+					<text x="155" y="22" fill="#FFFFFF" text-anchor="end">เกี่ยวกับค่าย</text>
+				</svg>
+			</a> -->
+			<a href="">
+				<svg x="0px" y="0px" width="170px" height="26px" viewBox="0 0 170 26">
+					<polygon fill="#000000" fill="none" points="0,0 170,0 160,26 0,26">
+					</polygon>
+					<text x="155" y="22" fill="#FFFFFF" text-anchor="end">รายละเอียด</text>
+				</svg>
+			</a>
+			<a href="">
+				<svg x="0px" y="0px" width="170px" height="26px" viewBox="0 0 170 26">
+					<polygon fill="#000000" fill="none" points="0,0 170,0 160,26 0,26">
+					</polygon>
+					<text x="155" y="22" fill="#FFFFFF" text-anchor="end">ROBOT</text>
+				</svg>
+			</a>
+			<a href="">
+				<svg x="0px" y="0px" width="170px" height="26px" viewBox="0 0 170 26">
+					<polygon fill="#000000" fill="none" points="0,0 170,0 160,26 0,26">
+					</polygon>
+					<text x="155" y="22" fill="#FFFFFF" text-anchor="end">NETWORK</text>
+				</svg>
+			</a>
+			<a href="">
+				<svg x="0px" y="0px" width="170px" height="26px" viewBox="0 0 170 26">
+					<polygon fill="#000000" fill="none" points="0,0 170,0 160,26 0,26">
+					</polygon>
+					<text x="155" y="22" fill="#FFFFFF" text-anchor="end">กำหนดการ</text>
+				</svg>
+			</a>
+			<a href="">
+				<svg x="0px" y="0px" width="170px" height="26px" viewBox="0 0 170 26">
+					<polygon fill="#000000" fill="none" points="0,0 170,0 160,26 0,26">
+					</polygon>
+					<text x="155" y="22" fill="#FFFFFF" text-anchor="end">เเผนที่ & ติดต่อ</text>
+				</svg>
+			</a>
+			<a href="">
+				<svg x="0px" y="0px" width="170px" height="26px" viewBox="0 0 170 26">
+					<polygon fill="#000000" fill="none" points="0,0 170,0 160,26 0,26">
+					</polygon>
+					<text x="155" y="22" fill="#FFFFFF" text-anchor="end">คำถาม</text>
+				</svg>
+			</a>
+			<a href="">
+				<svg x="0px" y="0px" width="170px" height="26px" viewBox="0 0 170 26">
+					<polygon fill="#000000" fill="none" points="0,0 170,0 160,26 0,26">
+					</polygon>
+					<text x="155" y="22" fill="#FFFFFF" text-anchor="end">บรรยากาศ</text>
+				</svg>
+			</a>
+			<a href="">
+				<svg x="0px" y="0px" width="170px" height="26px" viewBox="0 0 170 26">
+					<polygon fill="#000000" fill="none" points="0,0 170,0 160,26 0,26">
+					</polygon>
+					<text x="155" y="18" fill="#FFFFFF" text-anchor="end">ผู้สนับสนุน</text>
+				</svg>
+			</a>
+
+		</div> 
+		<div class="logo" >
+			<svg style="transform: translate3d(0px, 0px, 0px);" data-type="vertical_parallax" data-speed="2" x="0px" y="0px" width="492px" height="172px" viewBox="0 0 492 172">
+				<polygon fill="none" points="884.732,0 491.956,0 0,0 -394.333,0 -394.333,242.154 884.732,242.154 "></polygon>
+				<g>
+					<polyline fill="#333" points="0,-0.002 245.7,172.252 491.956,0 	"></polyline>
+					<polygon fill="#f00" points="486.236,4 5.708,4 0,0 491.956,0 	"></polygon>
+				</g>
+			</svg>
+		</div>
+		<div class="bg" id="bg">
+			<!-- about -->
+			<div class="top detail" >
+					<div class="content">
+						<div class="img left"></div>
+						<div class="text right">
+							<span class="head">รายละเอียดค่าย</span><hr>
+							<span class="body">&nbsp;&nbsp;&nbsp;ค่าย CE Smart Camp #8 จัดขึ้นในวันที่ 27 พฤษภาคม - 1 มิถุนายน 2558 (พักค้างแรม) โดยรับน้อง ม.ปลายจำนวน 80 คนจากทั่วประเทศ แบ่งเป็นสาขา <a href="">Robot</a> 40 คน และสาขา <a href="">Network</a> 40 คน
+								ซึ่งน้องๆสามารถเลือกอบรมสาขาใดก็ได้ การอบรมเน้นทั้งทฤษฎี และปฏิบัติ กับอุปกรณ์จริง...
+							</span>
+						</div>
+					</div>
+				<svg x="0px" y="0px" width="1920px" height="2700px" viewBox="0 0 1920 2700">
+					<polygon fill="#343434" points="1920,0 1920,1300 0,2700 0,1400"></polygon>
+				</svg>
+			</div>
+			<!-- detail -->
+			<div class="down  robot">
+				<div class="content">
+					<div class="img right"></div>
+					<div class="text left">
+						<span class="head">รายละเอียด ROBOT</span><hr>
+						<span class="body">&nbsp;&nbsp;&nbsp;ในส่วนของการอบรมการเขียนโปรแกรมควบคุมหุ่นยนต์ (Robot) น้องๆ จะได้เรียนรู้เกี่ยวกับกลไกการทำงานของหุ่นยนต์ แนวคิดการแก้ไขปัญหาอย่างมีระบบ และการเขียนโปรแกรมเพื่อควมคุมหุ่นยนต์เบื้องต้น ด้วยกิจกรรมสนุกๆ จากพี่ๆ ทั้งในรูปแบบทฤษฏี และปฏิบัติ น้องๆจะได้สัมผัสและทดลองจริงกับอุปกรณ์ชั้นนำ อาทิเช่น Lego mindstorms, Robobuilder ฯลฯ ...
+						</span>
+					</div>
+				</div>
+				<svg x="0px" y="0px" width="1920px" height="2700px" viewBox="0 0 1920 2700">
+					<polygon fill="#E52420" points="0,0 1920,1400 1920,2700 0,1300"></polygon>
+				</svg>			
+			</div>
+			<!-- robot -->
+			<div class="top  network">
+				<div class="content">
+					<div class="img left"></div>
+					<div class="text right">
+						<span class="head">รายละเอียด Network</span><hr>
+						<span class="body">&nbsp;&nbsp;&nbsp;ในโลกที่เราใช้คอมพิวเตอร์ในการสื่อสารกันอยู่ตลอดเวลา น้องๆเคยสงสัยไหมว่า ระบบเครือข่ายคอมพิวเตอร์นี่มันทำงานยังไงกันนะ? ถ้าอยากรู้ล่ะก็ เชิญมาหาคำตอบกันในค่าย Network เลย ภายในค่าย น้องๆจะได้รับความรู้ทางทฤษฎีพื้นฐานเกี่ยวกับระบบเครือข่ายคอมพิวเตอร์ เบื้องต้น นอกจากนั้นแล้ว น้องๆยังจะได้เรียนรู้การทำงานและการตั้งค่าอุปกรณ์ที่ใช้ในระบบเครือข่าย ผ่านการปฏิบัติจริงโดยใช้ชุดเครื่องมือที่ทันสมัยจาก Cisco...
+						</span>
+					</div>
+				</div>
+				<svg x="0px" y="0px" width="1920px" height="2700px" viewBox="0 0 1920 2700">
+					<polygon fill="#343434" points="1920,0 1920,1300 0,2700 0,1400"></polygon>
+				</svg>
+			</div>
+			<!-- Network -->
+			<div  class="down timeline">
+				<div class="content">
+					<div class="img right"></div>
+					<div class="text left">
+						<span class="head">กำหนดการ</span><hr>
+						<span class="body">
+							วันรับสมัคร : สมัครวันนี้ - 15 เมษายน 2558<br>
+							ประกาศผล : xxx<br>
+							ช่วงเวลาค่าย : 27 พฤษภาคม - 1 มิถุนายน 2558
+						</span>
+					</div>
+				</div>
+				<svg x="0px" y="0px" width="1920px" height="2700px" viewBox="0 0 1920 2700">
+					<polygon fill="#E52420" points="0,0 1920,1400 1920,2700 0,1300"></polygon>
+				</svg>
+			</div>
+			<!-- time line -->
+			<div class="top map_contect">
+				<div class="content">
+					<div class="img left"></div>
+					<div class="text right">
+						<span class="head">เเผนที่และ ติดต่อ</span><hr>
+						<span class="body">
+							พี่ปู้น : Tel(XXX-XXXXXXX), Facebook(<a href="">POON SEXY</a>)<br>
+							พี่เฟิร์ส : Tel(XXX-XXXXXXX), Facebook(<a href="">FIRST SEXY</a>)<br>
+							พี่ไอซ์ : Tel(XXX-XXXXXXX), Facebook(<a href="">ICE SEXY</a>)<br>
+							ที่อยู่ : อาคารปฏิบัติการรวม คณะวิศวกรรมศาสตร์ หลังที่ 2 (ECC) สาขาวิชาวิศวกรรมคอมพิวเตอร์ คณะวิศวกรรมศาสตร์ สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง เลขที่ 1 ซองฉลองกรุง 1 เขตลาดกระบัง แขวงลาดกระบัง กรุงเทพฯ 10520
+						</span>
+					</div>
+				</div>
+				<svg x="0px" y="0px" width="1920px" height="2700px" viewBox="0 0 1920 2700">
+					<polygon fill="#343434" points="1920,0 1920,1300 0,2700 0,1400"></polygon>
+				</svg>
+			</div>
+			<!-- map contact -->
+			<div  class="down faq">
+				<div class="content">
+					<div class="img right"></div>
+					<div class="text left">
+						<span class="head">คำถาม</span><hr>
+						<span class="body">
+							ถาม : วัน/เวลา/สถานที่ดำเนินกิจกรรม ?<br>
+							<span style="font-size:20px">
+								ตอบ : ระยะเวลาในการเข้าค่ายตั้งแต่วันที่ 27 พฤษภาคม - 1 มิถุนายน 2558 เป็นค่ายพักค้างแรม (ไม่อนุญาตให้เดินทางไปกลับ) สถานที่ดำเนินกิจกรรมทั้งหมดจัดภายในคณะวิศวกรรมศาสตร์ สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง<br><br>
+							</span>
+							ถาม : ไมมีบัตรประจำตัวประชาชน ต้องทำอย่างไร ?<br>
+							<span style="font-size:20px">
+								ตอบ : ใช้บัตรนักเรียนก็ได้จ้า<br><br>
+							</span>
+							ถาม : หากเกรดเฉลี่ยเทอมล่าสุดยังไม่ออก <br>ใช้เกรดเฉลี่ยเทอมไหน ?<br>
+							<span style="font-size:20px">
+								ตอบ : เกรดเฉลี่ยของเทอมล่าสุดที่เกรดออกเลยค่ะ ^^<br><br>
+							</span>
+							ถาม : หากกรอกผิด และกดยืนยันประวัติส่วนตัวไปแล้ว <br>จะสามารถแก้ได้หรือเปล่า ?<br>
+							<span style="font-size:20px">
+								ตอบ : ได้ค่ะ โดยจะเขียนใหม่ผ่านระบบบนเว็บไซต์ได้เลยจ้ะ    
+							</span>
+						</span>
+					</div>
+				</div>
+				<svg x="0px" y="0px" width="1920px" height="2700px" viewBox="0 0 1920 2700">
+					<polygon fill="#E52420" points="0,0 1920,1400 1920,2700 0,1300"></polygon>
+				</svg>
+			</div>
+			<!-- pic -->
+			<div class="top pic">
+				<div class="content" style="padding-top:40px">
+					<span class="head" >บรรยากาศ</span><hr>
+					<table width="100%" border="1" cellspacing="5px" height="400px">
+						<tr>
+							<td>ss</td>
+							<td>ss</td>
+							<td>ss</td>
+							<td>ss</td>
+							<td>ss</td>
+						</tr>
+						<tr>
+							<td>ss</td>
+							<td>ss</td>
+							<td>ss</td>
+							<td>ss</td>
+							<td>ss</td>
+						</tr>
+						<tr>
+							<td>ss</td>
+							<td>ss</td>
+							<td>ss</td>
+							<td>ss</td>
+							<td>ss</td>
+						</tr>
+						<tr>
+							<td>ss</td>
+							<td>ss</td>
+							<td>ss</td>
+							<td>ss</td>
+							<td>ss</td>
+						</tr>
+					</table>
+				</div>
+				<svg x="0px" y="0px" width="1920px" height="2700px" viewBox="0 0 1920 2700">
+					<polygon fill="#343434" points="1920,0 1920,1300 0,2700 0,1400"></polygon>
+				</svg>
+			</div>
+			<!-- spon -->
+			<div  class="down spon">
+				<div class="content" style="padding-top:40px" >
+					<span class="head" >ผ้สนับสนุน</span><hr>
+					<div align="center" style="width:100%">
+						<img src="sss">
+						<img src="sss">
+						<img src="sss">
+						<img src="sss">
+						<img src="sss">
+						<img src="sss">
+						<img src="sss">
+						<img src="sss">
+						<img src="sss">
+						<img src="sss">
+						<img src="sss">
+						<img src="sss">
+						<img src="sss">
+						<img src="sss">
+						<img src="sss">
+						<img src="sss">
+						<img src="sss">
+						<img src="sss">
+						<img src="sss">
+						<img src="sss">
+					</div>
+				</div>
+				<svg x="0px" y="0px" width="1920px" height="2700px" viewBox="0 0 1920 2700">
+					<polygon fill="#E52420" points="0,0 1920,1400 1920,2700 0,1300"></polygon>
+				</svg>
+			</div>
+			<div  class="down foot" >
+						ssss
+			</div>
+
+		</div>
+
+	</body>
+</html>
---
+++ index2.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;" />
+	<meta name="language" content="th" />
+	<title>CE SMART CAMP 8</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body >
+	<div class="nv_group">
+		<a href="">
+			<svg x="0px" y="0px" width="170px" height="26px" viewBox="0 0 170 26">
+				<polygon fill="#000000" fill="none" points="0,0 170,0 160,26 0,26">
+				</polygon>
+				<text x="155" y="22" fill="#FFFFFF" text-anchor="end">CE SMART CAMP 8</text>
+			</svg>
+		</a>
+<!-- 		<a href="">
+			<svg x="0px" y="0px" width="170px" height="26px" viewBox="0 0 170 26">
+				<polygon fill="#000000" fill="none" points="0,0 170,0 160,26 0,26">
+				</polygon>
+				<text x="155" y="22" fill="#FFFFFF" text-anchor="end">เกี่ยวกับค่าย</text>
+			</svg>
+		</a> -->
+		<a href="">
+			<svg x="0px" y="0px" width="170px" height="26px" viewBox="0 0 170 26">
+				<polygon fill="#000000" fill="none" points="0,0 170,0 160,26 0,26">
+				</polygon>
+				<text x="155" y="22" fill="#FFFFFF" text-anchor="end">รายละเอียด</text>
+			</svg>
+		</a>
+		<a href="">
+			<svg x="0px" y="0px" width="170px" height="26px" viewBox="0 0 170 26">
+				<polygon fill="#000000" fill="none" points="0,0 170,0 160,26 0,26">
+				</polygon>
+				<text x="155" y="22" fill="#FFFFFF" text-anchor="end">ROBOT</text>
+			</svg>
+		</a>
+		<a href="">
+			<svg x="0px" y="0px" width="170px" height="26px" viewBox="0 0 170 26">
+				<polygon fill="#000000" fill="none" points="0,0 170,0 160,26 0,26">
+				</polygon>
+				<text x="155" y="22" fill="#FFFFFF" text-anchor="end">NETWORK</text>
+			</svg>
+		</a>
+		<a href="">
+			<svg x="0px" y="0px" width="170px" height="26px" viewBox="0 0 170 26">
+				<polygon fill="#000000" fill="none" points="0,0 170,0 160,26 0,26">
+				</polygon>
+				<text x="155" y="22" fill="#FFFFFF" text-anchor="end">กำหนดการ</text>
+			</svg>
+		</a>
+		<a href="">
+			<svg x="0px" y="0px" width="170px" height="26px" viewBox="0 0 170 26">
+				<polygon fill="#000000" fill="none" points="0,0 170,0 160,26 0,26">
+				</polygon>
+				<text x="155" y="22" fill="#FFFFFF" text-anchor="end">เเผนที่ & ติดต่อ</text>
+			</svg>
+		</a>
+		<a href="">
+			<svg x="0px" y="0px" width="170px" height="26px" viewBox="0 0 170 26">
+				<polygon fill="#000000" fill="none" points="0,0 170,0 160,26 0,26">
+				</polygon>
+				<text x="155" y="22" fill="#FFFFFF" text-anchor="end">คำถาม</text>
+			</svg>
+		</a>
+		<a href="">
+			<svg x="0px" y="0px" width="170px" height="26px" viewBox="0 0 170 26">
+				<polygon fill="#000000" fill="none" points="0,0 170,0 160,26 0,26">
+				</polygon>
+				<text x="155" y="22" fill="#FFFFFF" text-anchor="end">บรรยากาศ</text>
+			</svg>
+		</a>
+		<a href="">
+			<svg x="0px" y="0px" width="170px" height="26px" viewBox="0 0 170 26">
+				<polygon fill="#000000" fill="none" points="0,0 170,0 160,26 0,26">
+				</polygon>
+				<text x="155" y="18" fill="#FFFFFF" text-anchor="end">ผู้สนับสนุน</text>
+			</svg>
+		</a>
+
+	</div> 
+	<div class="logo" >
+		<svg style="transform: translate3d(0px, 0px, 0px);" data-type="vertical_parallax" data-speed="2" x="0px" y="0px" width="492px" height="172px" viewBox="0 0 492 172">
+			<polygon fill="none" points="884.732,0 491.956,0 0,0 -394.333,0 -394.333,242.154 884.732,242.154 "></polygon>
+			<g>
+				<polyline fill="#333" points="0,-0.002 245.7,172.252 491.956,0 	"></polyline>
+				<polygon fill="#f00" points="486.236,4 5.708,4 0,0 491.956,0 	"></polygon>
+			</g>
+		</svg>
+	</div>
+	<div class="bg">
+		<!-- <img src="x.png"> -->
+		<svg x="0px" y="0px" width="2000px" height="200px" viewBox="0 0 2000 200">
+			<polygon fill="red" points="0,0 2000,0 2000,200 0,200"></polygon>
+		</svg>
+	</div>
+
+</body>
+
+</html>
